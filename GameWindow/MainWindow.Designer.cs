44a1dc238070e171bb80e1e3348a60c0c388fceb
Tommy Stenberg <stenberg.tommy@gmail.com>
2018-03-11 22:32:27 +0100

    Add project files.

---
+++ GameWindow/MainWindow.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace GameWindow
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tb_Status = new System.Windows.Forms.TextBox();
+            this.ballTimer = new System.Windows.Forms.Timer(this.components);
+            this.tb_Score = new System.Windows.Forms.TextBox();
+            this.Score = new System.Windows.Forms.Label();
+            this.tb_Lives = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.playfield = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.batter = new System.Windows.Forms.PictureBox();
+            this.playfield.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.batter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_Status
+            // 
+            this.tb_Status.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_Status.Enabled = false;
+            this.tb_Status.ForeColor = System.Drawing.Color.OrangeRed;
+            this.tb_Status.Location = new System.Drawing.Point(606, 338);
+            this.tb_Status.Name = "tb_Status";
+            this.tb_Status.Size = new System.Drawing.Size(155, 13);
+            this.tb_Status.TabIndex = 6;
+            // 
+            // ballTimer
+            // 
+            this.ballTimer.Enabled = true;
+            this.ballTimer.Interval = 50;
+            this.ballTimer.Tick += new System.EventHandler(this.BallTimer_Tick);
+            // 
+            // tb_Score
+            // 
+            this.tb_Score.Enabled = false;
+            this.tb_Score.Location = new System.Drawing.Point(606, 22);
+            this.tb_Score.Name = "tb_Score";
+            this.tb_Score.Size = new System.Drawing.Size(100, 20);
+            this.tb_Score.TabIndex = 7;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(566, 25);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(35, 13);
+            this.Score.TabIndex = 8;
+            this.Score.Text = "Score";
+            // 
+            // tb_Lives
+            // 
+            this.tb_Lives.Enabled = false;
+            this.tb_Lives.Location = new System.Drawing.Point(606, 80);
+            this.tb_Lives.Name = "tb_Lives";
+            this.tb_Lives.Size = new System.Drawing.Size(35, 20);
+            this.tb_Lives.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(566, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Lives";
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.Location = new System.Drawing.Point(569, 383);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(192, 42);
+            this.btn_Restart.TabIndex = 11;
+            this.btn_Restart.Text = "Play";
+            this.btn_Restart.UseVisualStyleBackColor = false;
+            this.btn_Restart.Click += new System.EventHandler(this.Btn_Play_Click);
+            // 
+            // playfield
+            // 
+            this.playfield.BackColor = System.Drawing.Color.MintCream;
+            this.playfield.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.playfield.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.playfield.Controls.Add(this.ball);
+            this.playfield.Controls.Add(this.batter);
+            this.playfield.Location = new System.Drawing.Point(29, 22);
+            this.playfield.Name = "playfield";
+            this.playfield.Size = new System.Drawing.Size(527, 403);
+            this.playfield.TabIndex = 1;
+            this.playfield.MouseEnter += new System.EventHandler(this.WhenTheMouseEnter);
+            this.playfield.MouseLeave += new System.EventHandler(this.WhenTheMouseLeave);
+            this.playfield.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveBat);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = global::GameWindow.Properties.Resources.tennis_PNG10416;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ball.InitialImage = null;
+            this.ball.Location = new System.Drawing.Point(178, 130);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 25);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // batter
+            // 
+            this.batter.BackColor = System.Drawing.Color.Black;
+            this.batter.Location = new System.Drawing.Point(214, 378);
+            this.batter.Name = "batter";
+            this.batter.Size = new System.Drawing.Size(118, 24);
+            this.batter.TabIndex = 0;
+            this.batter.TabStop = false;
+            this.batter.MouseEnter += new System.EventHandler(this.WhenTheMouseEnter);
+            this.batter.MouseLeave += new System.EventHandler(this.WhenTheMouseLeave);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Restart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Lives);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.tb_Score);
+            this.Controls.Add(this.tb_Status);
+            this.Controls.Add(this.playfield);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainWindow_KeyPress);
+            this.playfield.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.batter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox batter;
+        private System.Windows.Forms.Panel playfield;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.TextBox tb_Status;
+        private System.Windows.Forms.Timer ballTimer;
+        private System.Windows.Forms.TextBox tb_Score;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.TextBox tb_Lives;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Restart;
+    }
+}
+
---
+++ GameWindow/MainWindow.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWindow
+{
+    public partial class MainWindow : Form
+    {
+        #region fields
+
+        private bool isPaused;
+
+        private int xSpeed = 10;
+        private int ySpeed = -10;
+        public int xPos = 0;
+        public int yPos = 0;
+        public int livesLeft = 3;
+        public int score = 0;
+
+        #endregion
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            InitializeGame();
+            isPaused = true;
+            GamePaused();
+        }
+
+        private void InitializeGame()
+        {
+            xPos = batter.Location.X;
+            yPos = batter.Location.Y;
+            tb_Lives.Text = livesLeft.ToString();
+            tb_Score.Text = score.ToString();
+        }
+
+        public void MoveBat(object sender, MouseEventArgs e)
+        {
+            xPos = e.X;
+
+            if (!isPaused && xPos < (playfield.Width - batter.Width))
+                batter.Location = new Point(xPos, yPos);
+        }
+
+        private void WhenTheMouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void WhenTheMouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+
+        private void MainWindow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var key = e.KeyChar;
+
+            const char p = 'p';
+
+            switch (key)
+            {
+                case p:
+                    isPaused = !isPaused;
+                    GamePaused();
+                    break;
+            }
+        }
+
+        private void Restart()
+        {
+            xSpeed = 10;
+            ySpeed = 10;
+        }
+
+        private void GamePaused()
+        {
+            if (isPaused == true)
+            {
+                Cursor.Show();
+                tb_Status.Text = "Paused";
+            }
+            else
+            {
+                Cursor.Hide();
+                tb_Status.Clear();
+            }
+        }
+
+
+        // If the ball moves past the batter, you lose one life.
+        private bool LifeLost(Point location)
+        {
+            if (location.Y > playfield.Height - batter.Height)
+            {
+                livesLeft--;
+                UpdateGameStatus();
+                return true;
+            }
+            else return false;
+        }
+
+        private void BallTimer_Tick(object sender, EventArgs e)
+        {
+            var location = ball.Location;
+            var lost = LifeLost(ball.Location);
+
+            if (!isPaused)
+            {
+                if (lost)
+                {
+                    if (livesLeft == 0)
+                    {
+                        isPaused = true;
+                        GamePaused();
+                        tb_Status.Text = "Game Over!";
+                        livesLeft = 3;
+                    }
+                    var rnd = new Random().Next(ball.Width * 2, playfield.Width - 2 * ball.Width);
+                    ball.Location = new Point(rnd, 25);
+                }
+
+                else if (!CollidedWithBat(location))
+                {
+                    if ((location.X >= playfield.Width - ball.Width || location.X <= ball.Width))
+                    {
+                        xSpeed = -xSpeed;
+                    }
+                    if (location.Y <= ball.Height)
+                    {
+                        ySpeed = -ySpeed;
+                    }
+                }
+
+                else ySpeed = -ySpeed;
+                ball.Location = new Point(ball.Location.X + xSpeed, ball.Location.Y + ySpeed);
+            }
+        }
+
+        // Updates the game's score and life count.
+        private void UpdateGameStatus()
+        {
+            tb_Lives.Text = livesLeft.ToString();
+            tb_Score.Text = score.ToString();
+        }
+
+        // Checks if the ball collides with the batter.
+        private bool CollidedWithBat(Point location)
+        {
+            if (((location.X > batter.Location.X && location.X < batter.Location.X + batter.Width)
+                 || (location.X + ball.Width > batter.Location.X
+                 && location.X + ball.Width < batter.Location.X + batter.Width))
+                 && ball.Bottom + ball.Height/3 > batter.Top)
+            {
+                score++;
+                UpdateGameStatus();
+                return true;
+            }
+            return false;
+        }
+
+        private void Btn_Play_Click(object sender, EventArgs e)
+        {
+            isPaused = false;
+            GamePaused();
+            ball.Location = new Point(new Random().Next(ball.Width * 2, playfield.Width - 2 * ball.Width), 2 * ball.Height);
+            livesLeft = 3;
+            score = 0;
+            UpdateGameStatus();
+            playfield.Focus();
+        }
+    }
+}
